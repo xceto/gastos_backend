7d35e663d6c1e4adc58560ecbf265bc2460d2f54
claudio-wareclouds <ctroncoso@wareclouds.com>
2026-06-14 13:18:33 -0400

    remove defautl connection

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
+const sequelize = new Sequelize({ 
   dialect: 'postgres',
-  host:     process.env.DB_HOST     || 'localhost',
-  port:     parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USER     || 'conteo',
-  password: process.env.DB_PASSWORD || 'conteo_local_pw',
-  database: process.env.DB_NAME     || 'conteo_gastos',
+  host:     process.env.DB_HOST  ,
+  port:     parseInt(process.env.DB_PORT ),
+  username: process.env.DB_USER  ,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME ,
   logging: false,
   dialectOptions: process.env.NODE_ENV === 'production' ? {
     ssl: {
